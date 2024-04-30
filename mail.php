f36673b91654226d4cd4067b032804ef5241e73f
siva-rithish <sivasanthur1234@gmail.com>
2024-04-30 14:56:09 +0530

    mail

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@ $subject = $_POST["subject"];
 $message = $_POST["message"];
 
 
-$EmailTo = "contact.rithish@gmail.com";
+$EmailTo = "contact.rithishkar@gmail.com";
 $Title = "New Message Received";
 
 // prepare email body text
